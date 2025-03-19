2b5458892e81402842f59cfddcb0c3cf543b767a
naveengitboi <naveenjangiti9@gmail.com>
2025-03-19 15:39:52 +0530

    done with tables

--- sliders.py
+++ sliders.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
+from tkinter import scrolledtext
 app = tk.Tk()
 app.title("Sliders")
 
@@ -12,5 +12,9 @@ scale.pack()
 progressBar = ttk.Progressbar(master=app, variable=scaleValue, maximum=25, orient="horizontal", length=300)
 progressBar.pack()
 
+
+
+scrolleTextBox = scrolledtext.ScrolledText(master=app, width=50, height=5)
+scrolleTextBox.pack()
 
 app.mainloop()
